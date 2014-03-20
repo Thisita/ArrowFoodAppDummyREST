320f92dda3c0a01fbfbcf147e1bf06903a775c09
thisita <thisita@outlook.com>
2014-03-20 17:17:39 -0400

    getCart route

---
+++ routes/getCart.js
@@ -0,0 +1,43 @@
+/*
+    ArrowFoodAppDummyREST
+    Copyright © 2014 Ian Zachary Ledrick, also known as Thisita.
+    
+    This file is part of ArrowFoodAppDummyREST.
+
+    ArrowFoodAppDummyREST is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ArrowFoodAppDummyREST is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ArrowFoodAppDummyREST.  If not, see <http://www.gnu.org/licenses/>.
+*/
+'use strict';
+
+var response = [
+  {
+    menuId: "1",
+    itemId: "1",
+    quantity: "1"
+  },
+  {
+    menuId: "1",
+    itemId: "2",
+    quantity: "2"
+  }
+];
+
+// Route handling function
+function cart(req, res) {
+  res.send(JSON.stringify(response));
+}
+
+// Export the route association function
+module.exports = function(app) {
+  app.get('/cart', cart);
+};
