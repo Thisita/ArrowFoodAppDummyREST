aba45f1064b6d99deb49a5714991f94b95577aa7
thisita <thisita@outlook.com>
2014-03-20 21:12:56 -0400

    postCartOrder route

---
+++ routes/postCartOrder.js
@@ -0,0 +1,35 @@
+/*
+    ArrowFoodAppDummyREST
+    Copyright © 2014 Ian Zachary Ledrick, also known as Thisita.
+    
+    This file is part of ArrowFoodAppDummyREST.
+
+    ArrowFoodAppDummyREST is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ArrowFoodAppDummyREST is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ArrowFoodAppDummyREST.  If not, see <http://www.gnu.org/licenses/>.
+*/
+'use strict';
+
+var response = {};
+
+// Route handling function
+function cartOrder(req, res) {
+  // This function would empty the cart and place an order
+  // it is not complete, there should be a JSON body
+  // with order info in it.
+  res.send(JSON.stringify(response));
+}
+
+// Export the route association function
+module.exports = function(app) {
+  app.post('/cart/order', cartOrder);
+};
