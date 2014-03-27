e37485302cc32f4b8bc2d854c7e46b57c48727d5
thisita <thisita@outlook.com>
2014-03-27 10:16:10 -0400

    Order schema creation

---
+++ database/order.js
@@ -0,0 +1,54 @@
+/*
+    ArrowFoodAppDummyREST
+    Copyright © 2014 Ian Zachary Ledrick, also known as Thisita.
+    
+    This file is part of ArrowFoodAppDummyREST.
+
+    ArrowFoodAppDummyREST is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ArrowFoodAppDummyREST is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ArrowFoodAppDummyREST.  If not, see <http://www.gnu.org/licenses/>.
+*/
+'use strict';
+
+var mongoose = require('mongoose');
+
+// Schema for status items
+var orderStatusSchema = new mongoose.Schema({
+  authority: String,
+  state: String,
+  createdOn: Date,
+});
+
+// Schema for the order
+var orderSchema = new mongoose.Schema({
+  username: String,
+  createdOn: Date,
+  billing: {
+    address1: String,
+    address2: String,
+    city: String,
+    state: String,
+    zip: String
+  },
+  shipping: {
+    address1: String,
+    address2: String,
+    city: String,
+    state: String,
+    zip: String
+  },
+  status: [orderStatusSchema],
+  cart: mongoose.model('Cart')
+});
+
+// Export the schema
+var Order = module.exports = mongoose.model('Order', orderSchema);
