e1100241a03aea11f82282589c574a8ad495e2f8
thisita <thisita@outlook.com>
2014-03-27 10:00:21 -0400

    Added password reset schema
    Schema for keeping track of password reset requests

---
+++ database/passwordReset.js
@@ -0,0 +1,34 @@
+/*
+    ArrowFoodAppDummyREST
+    Copyright © 2014 Ian Zachary Ledrick, also known as Thisita.
+    
+    This file is part of ArrowFoodAppDummyREST.
+
+    ArrowFoodAppDummyREST is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ArrowFoodAppDummyREST is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ArrowFoodAppDummyREST.  If not, see <http://www.gnu.org/licenses/>.
+*/
+'use strict';
+
+var mongoose = require('mongoose');
+
+// This is a schema for reseting passwords in the usual way
+// Nothing fancy, just a short time token
+var passwordResetSchema = new mongoose.Schema({
+  username: String,
+  token: String,
+  expiration: Date,
+  createdOn: Date
+});
+
+// Export the schema
+var PasswordReset = module.exports = mongoose.model('PasswordReset', passwordResetSchema);
