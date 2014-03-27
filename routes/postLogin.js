c0f80fa544e9dc491c350958d6e868282a13465e
thisita <thisita@outlook.com>
2014-03-27 14:02:54 -0400

    Implementation of login code and mongo
    Implementation of mongodb User model usage.
    Use of pbkdf2 for login

--- routes/postLogin.js
+++ routes/postLogin.js
@@ -19,19 +19,81 @@
 */
 'use strict';
 
+// Some includes
+var crypto = require('crypto');
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+// The response for a 200 will always be the same.
 var response = {};
+
+// Some parameters for the pbkdf2
+var rounds = 10000;
+var keylen = 2048;
+
+// Helper to encode base64
+function encode(text) {
+  return new Buffer(text, 'utf8').toString('base64');
+}
+
+// Helper decode base64
+function decode(text) {
+  // yay buffers
+  return new Buffer(text, 'base64').toString('utf8');
+}
+
+// Helper function for testing passwords
+function checkPassword(attempt, password, salt, cb) {
+  // we use pbkdf2 because thisita <3 it
+  pbkdf2(attempt, decode(salt), iterations, keylen, function(err, derivedKey) {
+    if(derivedKey) {
+      // encode the key
+      var encodedAttempt = encode(derivedKey);
+      // return the result of a check 
+      cb(encodedAttempt == password);
+    } else {
+      // log that there was some weird error
+      console.log('pbkdf2() error: ' + err.toString());
+      // something broke so check is false
+      cb(false);
+    }
+  });
+}
 
 // Route handling function
 function login(req, res) {
+  // Parse the body
   var json = JSON.parse(req.body);
+  // check the syntax
   if(json.username && json.password) {
-    if(json.username === 'username'
-      && json.password === 'password') {
-      res.send(JSON.stringify(response));
-    } else {
-      res.send(401);
-    }
+    User.findOne({'username': json.username}, function(err, user) {
+      if(user) {
+        checkPassword(json.password, user.password, user.salt, function(match) {
+          if(match) {
+            // log
+            console.log(user.username + ' logged in');
+            // tell the user it was successful
+            res.send(JSON.stringify(response));
+          } else {
+            // log the failure
+            console.log('Failed login attempt for ' + user.username + ' (invalid password)');
+            // tell the user we could not find
+            // why no access denied? because we don't want
+            // attackers to tell the difference from
+            // no account
+            res.send(404);
+          }
+        }
+      } else {
+        // log failure
+        console.log('Failed login attempt for ' + json.username + ' (invalid username)');
+        // if we get nothing tell the user
+        // we couldn't find it
+        res.send(404);
+      }
+    });
   } else {
+    // if the syntax is bad let the user know
     res.send(400);
   }
 }
