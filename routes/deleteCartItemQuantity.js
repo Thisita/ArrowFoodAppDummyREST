9bee741ae13811694ed0ac17eebfabb774518c7e
Jonathan <jhday001@louisville.edu>
2014-04-05 22:46:06 -0400

    Implemented mongoDB functionality

--- routes/deleteCartItemQuantity.js
+++ routes/deleteCartItemQuantity.js
@@ -23,24 +23,135 @@
 var mongoose = require('mongoose');
 var Cart = mongoose.model('Cart');
 
-var response = {};
-
-var menuId = 1;
-var menuItemId = 1;
-
 // Route handling function
 function cart(req, res) {
-  var quantity = parseInt(req.params.quantity);
-  if(!isNaN(quantity)) {
-    if(req.params.menuId == menuId
-      && req.params.itemId == itemId) {
-      res.send(JSON.stringify(response));
-    } else {
-      res.send(404);
-    }
-  } else {
-    res.send(400);
-  }
+	// Boolean for knowing if the item has been deleted
+	var deleted = false;
+	// Check if the user is signed in
+	if(req.session.authenticated) {
+		Cart.findOne({'username' : req.session.username}, function(err, cart) {
+			if(cart) {
+				// Check that the menu exists
+				Menu.findOne({'restaurant' : req.params.restaurant, 'name' : req.params.menu}, function(err, menu){
+					if(menu) {
+						for (var i = 0; i < menu.items.length; ++i) {
+							// Check that the item actually exists in the menu
+							if(menu.items[i].name == req.params.item) {
+								for (var j = 0; j < cart.items.length; ++j) {
+									// Check the quantity
+									if(req.session.cart.items[i].quantity > req.params.quantity) {
+										// Decrement quantity of there is more than one
+										cart.items[i].quantity -= req.params.quantity;
+										
+										// Save the cart
+										cart.markModified('items');
+										cart.save(function(err) {
+										 if(err) {
+											console.error("Error: Failed to delete cart item quantity [" + err + "]");
+											res.send(500);
+											} else {
+												// Send success
+												res.send('{"success":true}');
+											}
+										});
+										
+										// Set boolean to true to escape the other for loop
+										deleted = true;
+										break;
+									} else {
+										// Remove the item completely from the cart
+										cart.items.splice(i, 1);
+										
+										// Save the cart
+										cart.markModified('items');
+										cart.save(function(err) {
+										 if(err) {
+											console.error("Error: Failed to delete cart item quantity [" + err + "]");
+											res.send(500);
+											} else {
+												// Send success
+												res.send('{"success":true}');
+											}
+										});
+										
+										// Set boolean to true to escape the other for loop
+										deleted = true;
+										break;
+									}
+								}
+								// Break out if the item was decremented
+								if (deleted) {
+									break;
+								} else {
+									// If the item was not found in the cart
+									res.send(404);
+								}
+							}
+						}
+					} else {
+						// Item not found
+						res.send(404);
+					}
+				});
+			} else {
+				// Could not find the cart
+				res.send(404);
+			}
+		});
+	// Else not signed in, use the cart in the request
+	} else {
+		if(req.session.cart){
+			Menu.findOne({'restaurant' : req.params.restaurant, 'name' : req.params.menu}, function(err, menu){
+				if(menu) {
+					for (var i = 0; i < menu.items.length; ++i) {
+						// Check that the item actually exists in the menu
+						if(menu.items[i].name == req.params.item) {
+							for (var j = 0; j < req.session.cart.items.length ; ++j) {
+								// Check that item is actually in the cart
+								if(req.session.cart.items[j] == req.params.item) {
+									// Check the quantity
+									if(req.session.cart.items[i].quantity > req.params.quantity) {
+										// Decrement quantity of there is more than one
+										req.session.cart.items[i].quantity -= req.params.quantity;
+										
+										// Set boolean to true to escape the other for loop
+										deleted = true;
+										
+										// Send success
+										res.send('{"success":true}');
+										break;
+									} else {
+										// Remove the item completely from the cart
+										req.session.cart.items.splice(i, 1);
+										
+										// Set boolean to true to escape the other for loop
+										deleted = true;
+										
+										// Send success
+										res.send('{"success":true}');
+										break;
+									}
+								}
+							}
+							// Break out if the item was decremented
+							if (deleted) {
+								break;
+							} else {
+								// If the item was not found in the cart
+								res.send(404);
+							}
+						}
+					}
+				} else {
+					// Item not found
+					res.send(404);
+				}
+			});
+		} else {
+			// Could not find the cart
+			res.send(404);
+		}
+	}
 }
 
 // Export the route association function
