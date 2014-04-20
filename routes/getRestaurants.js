13bc85b41d33f6a7187fa34c75b6b65cfe0732a9
Ian Zachary Ledrick <thisita@outlook.com>
2014-04-20 00:14:49 -0400

    Implemented get Restaurants

---
+++ routes/getRestaurants.js
@@ -0,0 +1,41 @@
+/*
+    ArrowFoodAppREST
+    Copyright © 2014 Ian Zachary Ledrick, also known as Thisita.
+    
+    This file is part of ArrowFoodAppREST.
+
+    ArrowFoodAppREST is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ArrowFoodAppREST is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ArrowFoodAppREST.  If not, see <http://www.gnu.org/licenses/>.
+*/
+'use strict';
+
+// Mongoose imports
+var mongoose = require('mongoose');
+var Restaurant = mongoose.model('Restaurant');
+
+// Route handling function
+function restaurants(req, res) {
+  Restaurant.find({}, function(err, docs) {
+    if (docs) {
+      res.send(JSON.stringify(docs));
+    } else {
+      // Could not find the menus
+      res.send(404);
+    }
+  });
+}
+
+// Export the route association function
+module.exports = function(app) {
+  app.get('/restaurants', restaurants);
+};
