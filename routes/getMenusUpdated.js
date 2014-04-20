d24045e8f334a430b96488467c29d2d694d32361
Ian Zachary Ledrick <thisita@outlook.com>
2014-04-20 01:21:55 -0400

    Implemented get menus updated

---
+++ routes/getMenusUpdated.js
@@ -0,0 +1,47 @@
+/*
+    ArrowFoodAppREST
+    Copyright © 2014 Ian Zachary Ledrick, also known as Thisita.
+    
+    This file is part of ArrowFoodAppREST.
+
+    ArrowFoodAppREST is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ArrowFoodAppREST is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ArrowFoodAppREST.  If not, see <http://www.gnu.org/licenses/>.
+*/
+'use strict';
+
+// Mongoose imports
+var mongoose = require('mongoose');
+var Menu = mongoose.model('Menu');
+
+// Route handling function
+function menusUpdated(req, res) {
+  Menu.find({}, function(err, menus) {
+    if (menus) {
+      var updated = menus[0].updated;
+      for(var i = 1; i < menus.length; ++i) {
+        if(menus[i].updated > updated) {
+          updated = menus[i].updated;
+        }
+      }
+      res.send('{"updated":' + updated + '}');
+    } else {
+      // Could not find the menus
+      res.send(404);
+    }
+  });
+}
+
+// Export the route association function
+module.exports = function(app) {
+  app.get('/menus/updated', menusUpdated);
+};
