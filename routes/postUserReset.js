ce36728927bb2dc5398952bc718d0d490c98be94
Ian Zachary Ledrick <thisita@outlook.com>
2014-04-21 00:38:03 -0400

    Bug fix: Variable mismatch

--- routes/postUserReset.js
+++ routes/postUserReset.js
@@ -89,7 +89,7 @@ function resetPassword(req, res) {
     } else {
       // Could not find user doc
       // log
-      console.log('ERROR: Could not find user [' + passwordReset.username + ']');
+      console.log('ERROR: Could not find user [' + user.username + ']');
       // send not found error
       res.send(404);
     }
