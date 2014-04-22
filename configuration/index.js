44f1e58424d3defd77abc5930965a4d7cc392a65
Ian Zachary Ledrick <thisita@outlook.com>
2014-04-22 12:19:25 -0400

    Bug fix: incorrectly setting sessionAge

--- configuration/index.js
+++ configuration/index.js
@@ -29,7 +29,7 @@ module.exports = function(app, express) {
   app.use(require('cookie-parser')());
   app.use(require('express-session')({
     secret: 'random_data',
-    maxAge: new Date(Date.now() + 3600000),
+    maxAge: 100 * 60 * 60 * 24,
     store: new MongoStore({
       url: url
     })
