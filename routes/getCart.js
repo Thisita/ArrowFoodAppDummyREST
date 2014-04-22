6301cc87524b9716b67b32219c6a6e07185693fa
Ian Zachary Ledrick <thisita@outlook.com>
2014-04-22 13:46:07 -0400

    Bug fix: cart.items[..].total not being updated

--- routes/getCart.js
+++ routes/getCart.js
@@ -22,18 +22,49 @@
 // Mongoose imports
 var mongoose = require('mongoose');
 var Cart = mongoose.model('Cart');
+var Menu = mongoose.model('Menu');
 
 // Route handling function
 function getCart(req, res) {
   if(req.session.authenticated) {
-    // Find the user's cart
-    Cart.findOne({'username' : req.session.username}, function(err, cart) {
-      if(cart) {
-        // Send the cart
-        res.send(JSON.stringify(cart));
+    // get the menus so we can update totals
+    Menu.find({}, function(err, menus) {
+      if(err) {
+        // log
+        console.log('ERROR: Could not get menus [' + err + ']');
+        // send the bad news
+        res.send(500, err);
+      } else if(menus) {
+        // Find the user's cart
+        Cart.findOne({'username' : req.session.username}, function(err, cart) {
+          if(cart) {
+            // update the prices
+            for(var i = 0; i < cart.items.length; ++i) {
+              for(var j = 0; j < menus.length; ++j) {
+                if(menus[j].name === cart.items[i].menu
+                  && menus[j].restaurant === cart.items[i]) {
+                  for(var k = 0; k < menus[j].items.length; ++k) {
+                    if(menus[j].items[k].name === cart.items[i].item) {
+                      cart.items[i].total = cart.items[i].quantity * menus[j].items[k].price;
+                      break;
+                    }
+                  }
+                  break;
+                }
+              }
+            }
+            // Send the cart
+            res.send(JSON.stringify(cart));
+          } else {
+            // Could not find the cart
+            res.send(404);
+          }
+        });
       } else {
-        // Could not find the cart
-        res.send(404);
+        // log
+        console.log('ERROR: No menus returned');
+        // send the bad news
+        res.send(404, 'No menus returned by MongoDB');
       }
     });
   } else {
@@ -42,8 +73,8 @@ function getCart(req, res) {
       // if it isn't there init it
       req.session.cart = {
         items: [],
-        updated: Date.now(),
-        created: Date.now()
+        updated: new Date(),
+        created: new Date()
       };
     }
     // send the session cart to the user
